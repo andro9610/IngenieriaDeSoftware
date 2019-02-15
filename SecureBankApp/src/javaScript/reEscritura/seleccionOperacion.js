115a1a26d82b9ee0cebd778830965d71fcc22fac
Andres Tovar <andro9610@gmail.com>
2019-02-14 19:09:44 -0500

    seleccionOperacion actualizada
    Se añade el boton para ingresar a la funcion ingresarDinero

--- SecureBankApp/src/javaScript/reEscritura/seleccionOperacion.js
+++ SecureBankApp/src/javaScript/reEscritura/seleccionOperacion.js
@@ -9,14 +9,16 @@ function escribirPantallaSeleccion(){
         '<button type="button" id="botonConsulta" class="botonSeleccion" onclick="escribirPantallaConsulta();">Consultar saldo</button>'+
         '<button type="button" id="botonRetiro" class="botonSeleccion" onclick="escribirPantallaCantidadRetiro();">Retirar dinero</button>'+
         '<button type="button" id="botonCambioClave" class="botonSeleccion" onclick="escribirPantallaCambioClave();">Cambiar clave</button>'+
+        '<button type="button" id="botonIngreso" class="botonSeleccion" onclick="escribirPantallaCantidadIngreso();">Ingresar dinero</button>'+
         '<h2 id="textoInferior"></h2>';
     
     let botonConsulta = document.getElementById('botonConsulta');
     let botonRetiro = document.getElementById('botonRetiro');
     let botonCambioClave = document.getElementById('botonCambioClave');
+    let botonIngreso = document.getElementById('botonIngreso');
 
     botonConsulta.style.backgroundImage = "url('../imagenes/consultaSaldo.png')";
     botonRetiro.style.backgroundImage = "url('../imagenes/retirarDinero.png')";
     botonCambioClave.style.backgroundImage = "url('../imagenes/key.png')";
+    botonIngreso.style.backgroundImage = "url('../imagenes/ingresarMoneda.png')";
 }
-
